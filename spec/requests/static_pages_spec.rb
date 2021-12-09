234863ad8cb24a0bab67a1aebfe430d193a66474
山根　直也 <yamamesan708@gmail.com>
2021-12-09 23:48:48 +0900

    static_pages/home add title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "レスポンスに成功" do
       get root_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
+    it "正しいタイトルが表示されている" do
+      get root_path
+      assert_select "title", "Home | dedama_jiman"
+    end
     
   end
 end
