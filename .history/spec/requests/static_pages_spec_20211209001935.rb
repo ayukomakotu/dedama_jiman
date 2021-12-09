fec646da2b426057db100ee96da2ff850b9163a8
山根　直也 <yamamesan708@gmail.com>
2021-12-09 20:50:52 +0900

    add static_pages_spec.rb

--- .history/spec/requests/static_pages_spec_20211209001935.rb
+++ .history/spec/requests/static_pages_spec_20211209001935.rb
@@ -4,9 +4,12 @@ RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "レスポンスに成功" do
       get root_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
-    
+    it "タイトルが正しく表示されている" do
+      get root_path
+      expect
+    end
   end
 end
--- .history/spec/requests/static_pages_spec_20211209002035.rb
+++ .history/spec/requests/static_pages_spec_20211209002035.rb
@@ -4,7 +4,7 @@ RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "レスポンスに成功" do
       get root_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
     
