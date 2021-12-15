669abe9543fc11d0d9e15bc6a9c80f39490bb65e
山根　直也 <yamamesan708@gmail.com>
2021-12-15 23:48:47 +0900

    user.rb validates追加 spec追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :michael, class: User do
+    name                     {"Michael Example"}
+    email                    {"michael@example.com"}
+    password                 {"password"}
+    password_confirmation    {"password"}
   end
 end
