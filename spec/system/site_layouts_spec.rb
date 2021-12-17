fa1450e2c91c00f59cf03ed9ad21186f07a172fe
山根　直也 <yamamesan708@gmail.com>
2021-12-17 21:54:53 +0900

    add sign_in link in header & spec

--- spec/system/site_layouts_spec.rb
+++ spec/system/site_layouts_spec.rb
@@ -3,14 +3,19 @@ require 'rails_helper'
 RSpec.describe "SiteLayouts", type: :system do
 
   context "headerのリンク" do
-    it "homeへのリンクが機能しているか" do
-      get root_path
-      expect(response.body).to have_link 'Home', href: root_path
+    it "homeへのリンクがあるか" do
+      visit root_path
+      expect(page).to have_link 'Home', href: root_path
     end
 
-    it "helpへのリンクが機能しているか" do
-      get root_path
-      expect(response.body).to have_link 'Help', href: help_path
+    it "helpへのリンクがあるか" do
+      visit root_path
+      expect(page).to have_link 'Help', href: help_path
+    end
+
+    it "sign_inへのリンクがあるか" do
+      visit root_path
+      expect(page).to have_link 'Sign in', href: new_user_session_path
     end
   end
 end
