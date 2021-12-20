cceb7bc355af97bf2bfc9e0abde04ceaf2e789df
山根　直也 <yamamesan708@gmail.com>
2021-12-20 23:11:40 +0900

    user.rb deviseオプション:confirmable development.rb メール設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,10 +35,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = { protocol: 'https', host: 'http://127.0.0.1:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto => true,
+    address => "smtp.gmail.com",
+    port => 587,
+    domain => 'smtp.gmail.com',
+    user_name => "xxx@gmail.com", #gmailアドレス
+    password => "xxxx", #gmailパスワード
+    authentication => 'login'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
