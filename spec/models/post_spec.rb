e28d463b7f28360394b4f76a14a7aea11805104a
山根　直也 <yamamesan708@gmail.com>
2021-12-26 16:29:39 +0900

    add post model & validatetion & spec

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @user = FactoryBot.create(:michael)
+    @post = Post.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  it "postが有効になるか" do
+    expect(@post).to be_valid
+  end
+
+  it "user_idが空で無効" do
+    @post.user_id = nil
+    expect(@post).to be_invalid
+  end
+
+  it "contentが140字以内" do
+    @post.content = "a"*141
+    expect(@post).to be_invalid
+  end
+end
