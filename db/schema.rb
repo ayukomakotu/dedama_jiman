37c79dc8c34ec4256388e1db7f1aa72131424838
山根　直也 <yamamesan708@gmail.com>
2021-12-28 22:50:41 +0900

    カラム名変更　view spec修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_12_28_065611) do
 
   create_table "machines", force: :cascade do |t|
-    t.text "kind"
+    t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
