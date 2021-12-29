3892014eab1f32df2eca29b7efa6515b501e8cf3
山根　直也 <yamamesan708@gmail.com>
2021-12-30 00:41:19 +0900

    spec修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
-
   factory :michael, class: User do
     name                     {"Michael Example"}
     email                    {"michael@example.com"}
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,45 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-  let!(:michael)               { create(:michael) }
-  let!(:test_classification)   { create(:test_classification) }
-  let!(:test_kind)             { create(:test_kind, classification: test_classification) }
-                                 
-  let!(:test_machine)          { create(:test_machine, kind: test_kind,
-                                                       classification: test_classification) }
-                                 
-  let!(:test_post)             { create(:test_post, user: michael,
-                                                    classification: test_classification,
-                                                    kind: test_kind,
-                                                    machine: test_machine) }
-
-  let!(:most_recent)           { create(:most_recent, user: michael,
-                                                      classification: test_classification,
-                                                      kind: test_kind,
-                                                      machine: test_machine) }
-  
-  
-  before do  
-    @post = michael.posts.build(content: "Lorem ipsum", machine: test_machine,
-                                                        kind: test_kind,
-                                                        classification: test_classification)
+  let!(:test_post)    { create(:test_post) }
+  before do
+    @post = test_post.user.posts.build(content: "valid_post",
+                                    user: test_post.user,
+                                 machine: test_post.machine,
+                                    kind: test_post.kind,
+                          classification: test_post.classification)
   end
 
-  it "postが有効になるか" do
-    expect(@post).to be_valid
+  context "validation" do
+    it "postが有効になるか" do
+      expect(@post).to be_valid
+    end
+
+    it "user_idが空で無効" do
+      @post.user_id = nil
+      expect(@post).to be_invalid
+    end
+
+    it "contentが140字以内" do
+      @post.content = "a"*141
+      expect(@post).to be_invalid
+    end
   end
 
-  it "user_idが空で無効" do
-    @post.user_id = nil
-    expect(@post).to be_invalid
-  end
-
-  it "contentが140字以内" do
-    @post.content = "a"*141
-    expect(@post).to be_invalid
-  end
-
-  it "投稿は作成時間の逆順で表示されるか" do
-    expect(Post.first).to eq most_recent
+  context "order" do               
+    it "投稿は作成時間の逆順で表示されるか" do
+      most_recent = Post.create(content: "most_recent",
+                                   user: test_post.user,
+                                machine: test_post.machine,
+                                   kind: test_post.kind,
+                         classification: test_post.classification)
+      expect(Post.first).to eq most_recent
+    end
   end
 end
