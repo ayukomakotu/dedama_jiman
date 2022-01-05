b22629e7560cc227cdb3126ad992d0b353e15526
山根　直也 <yamamesan708@gmail.com>
2022-01-05 20:07:34 +0900

    posts_spec修正

---
+++ .history/spec/system/index_users_spec_20220105200652.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "IndexUsers", type: :system do
+
+let!(:archer){FactoryBot.create(:archer)}
+
+  context "GET /users/index" do
+    it "ページネーションが機能しているか" do
+      create_list(:sample, 30)
+      visit users_path
+      click_link "Next", match: :first
+      expect(page).to have_content User.last.name
+    end
+  end
+end
