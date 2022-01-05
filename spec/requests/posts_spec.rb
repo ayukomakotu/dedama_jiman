274460732c0eb7bf879a600ddab14a33b699f585
山根　直也 <yamamesan708@gmail.com>
2022-01-05 20:07:34 +0900

    posts_spec修正

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  let!(:michael)      { FactoryBot.create(:michael) }
+  let!(:pachinko)     { FactoryBot.create(:pachinko) }
+  let!(:test_kind)    { FactoryBot.create(:test_kind, classification: pachinko) }
+  let!(:test_machine) { FactoryBot.create(:test_machine, kind: test_kind,
+                                                         classification: pachinko)}
+  
+
+  describe "POST/create" do
+    it "createした後 users/showにリダイレクトする" do
+      sign_in michael
+      get root_url
+      expect do
+        post posts_path, params: { post: {machine_id: test_machine.id,
+                                          content: "content"},
+                                  acquired: {number: 100} } 
+      end.to change(Post, :count).by(1)
+      expect(response).to have_http_status(302)
+    end
   end
 end
